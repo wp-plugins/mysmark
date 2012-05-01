57c9b5e244c9d3d9db309a3d90123bbf889dfe87
m1rcu2 <m1rcu2@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-01 02:03:47 +0000

    Added: Template and Width in plugin CP. Removed: Single vote because there's still an issue on back-end side. Minor changes.
    git-svn-id: https://plugins.svn.wordpress.org/mysmark/trunk@538397 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MySm_Plugin.php
+++ MySm_Plugin.php
@@ -3,7 +3,7 @@
 	Plugin Name: MySmark
 	Plugin URI: http://mysmark.com
 	Description: A MySmark Plug-In for WordPress CMS
-	Version: 1.0.1
+	Version: 1.0.2
 	Author: M1rcu2
 	Author URI: http://b-smark.com/mirco
 	License: GPL2
@@ -39,17 +39,19 @@
 
 	class MySmark_Plugin
 	{
-       
-    //It registers the plugin actions
+		
+		//It registers the plugin actions
 		function __construct()
 		{
 			register_activation_hook( __FILE__, array(  &$this, 'OnActivate' ) );
 			register_deactivation_hook( __FILE__, array(  &$this, 'OnDeactivate' ) );
+			register_uninstall_hook(__FILE__, array(  &$this, 'OnUninstall' ));
 			add_action('admin_menu', array(&$this, 'CreateMenu'));
 			add_filter('the_content', array(&$this, 'AppendWidget'));
 			add_action('publish_post', array(&$this, 'AddMySmReference'));
 			add_action('after_delete_post', array(&$this, 'RemoveOption'));
 			add_action('admin_notices', array(&$this, 'MySmWarning'));
+			add_action('wp_enqueue_scripts', array(&$this, 'EnableJScript'));
 		}
 		
 		
@@ -77,12 +79,27 @@
 		
 		function OnDeactivate()
 		{
-			delete_option('mysm-oauth-cli');
-			delete_option('mysm-oauth-secr');
+//			delete_option('mysm-oauth-cli');
+//			delete_option('mysm-oauth-secr');
 //			delete_option('mysm-template');
 //			delete_option('mysm-multimedia');
 //			delete_option('mysm-share');
 		}
+		
+		function OnUninstall()
+		{
+			delete_option('mysm-oauth-cli');
+			delete_option('mysm-oauth-secr');
+			delete_option('mysm-template');
+			delete_option('mysm-multimedia');
+			delete_option('mysm-share');
+			UnlinkAllPosts();
+		}
+		
+		function EnableJScript() {
+			wp_register_script( 'mysmscript', plugins_url('/js/mysmscript.js', __FILE__));
+			wp_enqueue_script( 'mysmscript' );
+		}    
 		
 		function MySmWarning()
 		{
@@ -135,17 +152,17 @@
 			if (is_single())
 			{
 				global $post;
-			        global $websrc;
+				global $websrc;
+				
 				$mysmID = get_option("MySmPostID".$post->ID);
 				try
 				{
 					if (empty($mysmID))
 					{
-						$singlevote = get_option('mysm-singlevote');
-						$mysmID = $this->MySmPOST($post->ID, $post->post_title, $post->guid, !empty($singlevote));
-						add_option("MySmPostID".$post->ID, $mysmID);
+						$mysmID = $this->MySmPOST($post->ID, $post->post_title, $post->guid);
+						update_option("MySmPostID".$post->ID, $mysmID);
 					}
-					$orientation = 'pos=b';
+					$orientation = 't';
 					$width = '230';
 					$height = '660';
 					
@@ -153,7 +170,7 @@
 					{
 						case 0:
 							{
-								$orientation = 'pos=l';
+								$orientation = 'l';
 								if (get_option('mysm-width'))
 									$width = get_option('mysm-width');
 								else
@@ -161,9 +178,19 @@
 								$height = '330';
 								break;
 							}
+						case 1:
+							{
+								$orientation = 't';
+								if (get_option('mysm-width'))
+									$width = get_option('mysm-width');
+								else
+									$width = 230;
+								$height = '330';
+								break;
+							}
 						case 2:
 							{
-								$orientation = 'pos=r';
+								$orientation = 'r';
 								if (get_option('mysm-width'))
 									$width = get_option('mysm-width');
 								else
@@ -173,13 +200,13 @@
 							}
 						default:
 							{
-								$orientation = 'pos=b';
+								$orientation = 't';
 								$height = '660';
 								break;
 							}
-					}		
-					
-					$content .= '<br/><br/><iframe id="mySmarkFrame" src="'.$websrc.'embed.php?id='.$mysmID.'&comm=1&wh='.$width.'&'.$orientation.'" height="'.($height+10).'" width="'.($width+5).'"></iframe>';
+					}
+					$wpurl = (($_SERVER['HTTPS'] != "on") ? "http://" : "https://").$_SERVER['HTTP_HOST'];
+					$content .= '<br/><br/><iframe id="mySmarkFrame" src="'.$websrc.'embed.php?id='.$mysmID.'&comm=1&wh='.$width.'&pos='.$orientation.'&exturl='.$wpurl.'" height="'.($height+10).'" width="'.($width+5).'"></iframe>';
 				}
 				catch (MySmarkException $e)
 				{
